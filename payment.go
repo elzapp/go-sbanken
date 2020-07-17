ea25dfd2f31d53215932d2c6318d5ddc07af6935
Bård Aase <elzapp@gmail.com>
2020-07-17 19:18:58 +0200

    add GetPayments

---
+++ payment.go
@@ -0,0 +1,49 @@
+package sbanken
+
+import (
+	"encoding/json"
+)
+
+const payments = `https://api.sbanken.no/exec.bank/api/v1/Payments/`
+
+// Payment ...
+type Payment struct {
+	ID                     string   `json:"paymentId"`
+	RecipientAccountNumber string   `json:"recipientAccountNumber"`
+	Amount                 float64  `json:"amount"`
+	DueDate                string   `json:"dueDate"`
+	KID                    string   `json:"kid"`
+	Text                   string   `json:"text"`
+	IsActive               bool     `json:"isActive"`
+	Status                 string   `json:"status"`
+	AllowedNewStatusTypes  []string `json:"allowedNewStatusTypes"`
+	StatusDetails          string   `json:"statusDetails"`
+	ProductType            string   `json:"productType"`
+	PaymentType            string   `json:"paymentType"`
+	PaymentNumber          int64    `json:"paymentNumber"`
+	BeneficiaryName        string   `json:"beneficiaryName"`
+}
+
+type paymentListResponse struct {
+	AvailableItems int64     `json:"availableItems"`
+	Items          []Payment `json:"items"`
+	errorInformation
+}
+
+type parymentItemResponse struct {
+	Item Payment `json:"item"`
+	errorInformation
+}
+
+// GetPayments ...
+func (conn *APIConnection) GetPayments(accountID string) ([]Payment, error) {
+	r := newAPIRequest()
+	r.target = payments + accountID
+	var a paymentListResponse
+	resp, err := conn.makeAPIRequest(r)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(resp, &a)
+	return a.Items, nil
+}
